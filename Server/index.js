4d8fc9b709cbeb18e55ce83167eeca85ea7e451f
Deepika-Binjola <deepikabinjola05@gmail.com>
2024-09-08 16:53:17 +0530

    add client-side

--- Server/index.js
+++ Server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: [""],
+		origin: ["https://studynotion-frontend-beta-five.vercel.app/"],
 		method:["POST","GET","PUT","DELETE","PATCH"],
 		credentials: true,
 	})
